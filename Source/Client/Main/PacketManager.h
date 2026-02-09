64d6881d70d9d08dbeac0be8384fc5dc056690ba
Emanuel B. Catania <emanuelb.catania@gmail.com>
2026-02-09 02:13:31 -0300

    client sources

---
+++ Source/Client/Main/PacketManager.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#define MAX_MAIN_PACKET_SIZE 2048
+
+#pragma pack(push, 1)
+struct ENCDEC_HEADER
+{
+	WORD header;
+	DWORD size;
+};
+#pragma pack(pop)
+
+static_assert(sizeof(ENCDEC_HEADER) == 6, "ENCDEC_HEADER must be packed to 6 bytes");
+
+struct ENCDEC_DATA
+{
+	DWORD Modulus[4];
+	DWORD Key[4];
+	DWORD Xor[4];
+};
+
+class CPacketManager
+{
+public:
+
+	CPacketManager();
+
+	virtual ~CPacketManager();
+
+	bool LoadEncryptionKey(char* name);
+
+	bool LoadDecryptionKey(char* name);
+
+	int Encrypt(BYTE* lpTarget, BYTE* lpSource, int size);
+
+	int Decrypt(BYTE* lpTarget, BYTE* lpSource, int size);
+
+	bool AddData(BYTE* lpBuff, int size);
+
+	bool ExtractPacket(BYTE* lpBuff);
+
+private:
+
+	void Init();
+
+	bool LoadKey(char* name, WORD header, bool type);
+
+	int EncryptBlock(BYTE* lpTarget, BYTE* lpSource, int size);
+
+	int DecryptBlock(BYTE* lpTarget, BYTE* lpSource);
+
+	int AddBits(BYTE* lpTarget, int TargetBitPos, BYTE* lpSource, int SourceBitPos, int size);
+
+	int GetByteOfBit(int value);
+
+	void Shift(BYTE* lpBuff, int size, int ShiftSize);
+
+	void XorData(int start, int end);
+
+private:
+
+	ENCDEC_DATA m_Encryption;
+
+	ENCDEC_DATA m_Decryption;
+
+	DWORD m_SaveLoadXor[4];
+
+	BYTE m_buff[MAX_MAIN_PACKET_SIZE];
+
+	DWORD m_size;
+
+	BYTE m_XorFilter[32];
+};
+
+extern CPacketManager gPacketManager;
